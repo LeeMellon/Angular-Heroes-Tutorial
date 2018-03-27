dc11dc7a988d4eec0841f017e26cea876fdb487a
Hamza Naeem and Ian Goodrich <ian@>
2018-03-27 11:13:25 -0700

    got it working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,12 @@ import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
+ exports: [ RouterModule ],
  imports: [ RouterModule.forRoot(routes) ],
-
- exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
